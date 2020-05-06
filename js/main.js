c18cc889baf4b274bfc575a79de9d0f3bf417df1
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-05-06 18:40:19 +0300

    count neighbours

--- js/main.js
+++ js/main.js
@@ -13,9 +13,10 @@ let grid;
 let cols = 20;
 let rows = 20;
 
-function setup () {
 
+function setup () {
     grid = make2DArray(cols, rows);
+
     for (let i=0; i<cols; i++) {
         for (let j=0; j<rows; j++) {            
             //grid[i][j] = floor(random(2));
@@ -26,8 +27,50 @@ function setup () {
             }
         }        
     }
-    //console.log(grid);
-    console.table(grid);
-    
+    console.table(grid);  
 }
+
+function nextGeneration () {
+    let next = make2DArray(cols, rows);
+    for (let i=0; i<cols; i++) {
+        for (let j=0; j<rows; j++) {
+            let state = grid[i][j];
+
+            //edges
+            if (i===0 || i===cols-1 || j===0 || j===rows-1){
+                next[i][j] = state;
+            } else {
+                //count live neighbours
+                let neighbours = countNeighbours(grid, i, j);            
+
+                if (state === '.' && neighbours === 3) {
+                    next[i][j] = 'X';
+                } else
+                if (state === 'X' && (neighbours < 2 || neighbours > 3)) {
+                    next[i][j] = '.';
+                } else {
+                    next[i][j] = state;
+                }
+            }
+        }
+    }
+    console.table(next);    
+}
+
+function countNeighbours (grid, x, y) {
+    let sum = 0;
+    for (let i=-1; i<2; i++) {
+        for (let j=-1; j<2; j++) {
+            if (grid[x+i][y+j] === 'X') {
+                sum += 1;
+            }
+        }
+    }
+    if (grid[x][y] === 'X') {
+        sum -= 1;
+    }        
+    return sum;
+}
+
 setup();
+nextGeneration ();
